044189028e7dbca05c167cd3a6ba2bde9f980f95
SHIOYA, Hiromu <kwappa.856@gmail.com>
2013-06-20 20:50:27 +0900

    [nintendo_cp] add example-answer for fourth page

--- nintendo_cp/q02/hourses_tour.rb
+++ nintendo_cp/q02/hourses_tour.rb
@@ -1,5 +1,34 @@
+require 'set'
+require_relative './utility'
+
 module HoursesTour
   class Solver
+    def initialize(grid)
+      @grid = grid
+      @path = []
+      @solutions = []
+    end
+
+    def can_move?(x, y, to_x, to_y)
+      movable_positions(x, y).include?([to_x, to_y])
+    end
+
+    def movable_positions(x, y)
+      positions = Set.new([[x + 1, y], [x - 1, y], [x, y + 1], [x, y - 1]])
+      1.upto(8) do |c|
+        positions << ([x + c, y + c])
+        positions << ([x - c, y + c])
+        positions << ([x + c, y - c])
+        positions << ([x - c, y - c])
+      end
+      positions.find_all { |position| in_grid?(*position) }
+    end
+
+    def solve(x, y)
+      solve_with_backtracking(x, y, @grid.cell(x, y).to_i)
+      @solutions
+    end
+
     def solve_with_backtracking(x, y, k)
       @path << [x, y]
       if k == 9
@@ -12,6 +41,12 @@ module HoursesTour
         }
       end
       @path.pop
+    end
+
+    private
+
+    def in_grid?(x, y)
+      x.between?(1, 9) && y.between?(1, 9)
     end
   end
 end
